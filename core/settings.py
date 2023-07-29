067421c571cb2801db8cd96a1953ebb5febbbade
José Lucas Torquato <jltorquato12@gmail.com>
2023-07-29 11:35:52 -0300

    fix: allowed host add *

--- core/settings.py
+++ core/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-d@!ndk(=r^y_%1vfheu@%z(l0z4y9t0@seuwih!wg_(9^qz_ri
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
