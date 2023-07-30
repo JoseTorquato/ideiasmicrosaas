39a491ba3c2bc7fa7171873ec803d0657922e16c
José Lucas Torquato <jltorquato12@gmail.com>
2023-07-30 20:58:18 -0300

    feat: site maps

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import home_view
+from .views import home_view,sitemap_view
 
 urlpatterns = [
-    path("", home_view, name="home")
+    path("", home_view, name="home"),
+    path("sitemap.xml", sitemap_view, name="sitemap"),
 ]
--- home/views.py
+++ home/views.py
@@ -26,3 +26,6 @@ def home_view(request):
 
     data = Ideas.objects.all().order_by('-created_at').values('microsaas_name', 'description', 'short_description', 'how_it_works', 'business_model', 'target_audience', 'competitive_edge')
     return render(request, 'index.html', context={ "ideas": data }) 
+
+def sitemap_view(request):
+    return render(request, 'sitemap.xml') 
